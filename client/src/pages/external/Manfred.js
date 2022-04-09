14e1a5da2cc140113c1b73eec9ece23b9902c387
codecat111 <codecat111@gmail.com>
2022-04-09 10:28:03 +0200

    omg light-mode på index/manfred asså ex clap ba

--- client/src/pages/external/Manfred.js
+++ client/src/pages/external/Manfred.js
@@ -5,23 +5,31 @@ import '../../styles/Manfred.scss'
 export default function Manfred() {
 
     const StartButtonRef = useRef();
+    const page_root = useRef();
 
-    function GetStarted(){
-        alert(StartButtonRef.current.value);
+    function ChangeToLightMode(){
+        const page_root_temp = page_root.current;
+        if(page_root_temp.classList.contains('light-mode')){
+            page_root_temp.className = '';
+            return;
+        }
+        page_root_temp.className = 'light-mode';
     }
 
     return (
-        <div>
+        <div ref={page_root}>
             <ExternalHeader />
             <div className="Manfred">
                 <div className="Manfred-container">
-                    <div className="head-container">
+                    <section className="head-container">
+                        <button onClick={ChangeToLightMode}>Lite light-mode experiment</button>
+
                         <h1>Learn about aktier ba</h1>
                         <h1>Earn money ba</h1>
                         <p>Hello here is some info about this thingy magiggy</p>
                         <span>Socials här ba</span>
-                        <button ref={StartButtonRef} onClick={GetStarted} value="hellos">Get startet ba buy a shrit ba</button>
-                    </div>
+                        <button ref={StartButtonRef} onClick={a => alert('abc123')}>Get startet ba buy a shrit ba</button>
+                    </section>
                 </div>
             </div>
         </div>
