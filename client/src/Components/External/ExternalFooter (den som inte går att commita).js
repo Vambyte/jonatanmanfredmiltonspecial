18eecdcdacea2d690679c0f7ec6600d5c83e3c7f
codecat111 <codecat111@gmail.com>
2022-04-09 15:24:31 +0200

    footer

---
+++ client/src/Components/External/ExternalFooter (den som inte går att commita).js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "../../styles/Components.scss"
+
+
+export default function ExternalFooter() {
+  return (
+    <div className="ExternalFooter">
+      <footer>
+        <span>&#169;Aktielabbet 2022</span>
+        <span>Socials</span>
+        <span>Contact kanske</span>
+      </footer>
+    </div>
+  )
+}
