5acb905c8bd5ebda78e5c5c6d133ba71fad57bf8
manjea <manjea@edu.umea.se>
2022-04-11 12:46:30 +0200

    footer

--- client/src/components/external/ExternalFooter.js
+++ client/src/components/external/ExternalFooter.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import "../../styles/Components.scss"
+
+//använd det här footern, det går inte att ändra den andra
+
 export default function ExternalFooter() {
   return (
-    <>
-        <footer>
-        </footer>
-    </>
+    <div className="ExternalFooter">
+      <footer>
+        <span>&#169;Aktielabbet 2022</span>
+        <span>Socials</span>
+        <span>Contact kanske</span>
+      </footer>
+    </div>
   )
 }
